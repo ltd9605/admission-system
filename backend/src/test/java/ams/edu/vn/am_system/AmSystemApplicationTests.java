7bca823b4aa08de7e6d78e97bc59ed90a47236ac
Silvester <ltd9605@gmail.com>
2026-08-03 03:49:43 +0700

    feat: Initialize Spring Boot backend core

---
+++ backend/src/test/java/ams/edu/vn/am_system/AmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ams.edu.vn.am_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
